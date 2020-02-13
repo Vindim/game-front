6c8e5d11f3a322adf386992ae3d3a8bf50fd1ef1
vindim-perco <Winnie260590>
2020-02-13 12:06:20 +0300

    next

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {Title} from '@angular/platform-browser';
 
 @Component({
-  selector: 'detectives-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'detectives-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+    loginForm: FormGroup;
 
-  ngOnInit() {
-  }
+    constructor(
+        private router: Router,
+        private title: Title
+    ) {
+        title.setTitle('Вход в систему');
+    }
+
+    ngOnInit() {
+        this.loginForm = new FormGroup({
+            email: new FormControl(null, [Validators.required, Validators.email]),
+            password: new FormControl(null, [Validators.required, Validators.minLength(6)])
+        });
+    }
+
+    onSubmit() {
+
+    }
 
 }
--- src/app/auth/registration/registration.component.ts
+++ src/app/auth/registration/registration.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Title} from '@angular/platform-browser';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'detectives-registration',
-  templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss']
+    selector: 'detectives-registration',
+    templateUrl: './registration.component.html',
+    styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+    registrationForm: FormGroup;
 
-  ngOnInit() {
-  }
+    constructor(
+        private title: Title,
+        private router: Router
+    ) {
+        title.setTitle('Регистрация');
+    }
+
+    ngOnInit() {
+        this.registrationForm = new FormGroup({
+            email: new FormControl(null, [Validators.required, Validators.email]),
+            password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+            confirmPassword: new FormControl(null, [Validators.required]),
+            name: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+            agree: new FormControl(false, [Validators.requiredTrue])
+        });
+    }
+
+    onSubmit() {
+
+    }
 
 }
